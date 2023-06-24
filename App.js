4707961d33e84cdd6d8bacc5b4bc31912bbe6367
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-24 09:31:35 +0200

    Added Pages

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return
+ (
+  <NavigationContainer>
+       <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#0080ff",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        headerTitleAlign: "center",
+      }}
+    >
+      <Stack.Screen name="Splash" 
+       options={{
+        headerShown: false,
+      }}
+      component={Splashcreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Menu" component={TopBar} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+ )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/DashboardScreen.js
+++ components/DashboardScreen.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { globalStyles } from "../styles/global";
-import Card from '../shared/card'
+import Card from "../shared/card";
 
-export default function HomeScreen(){
-    return(
-        <View style={globalStyles.container}>
-            <Card>
-            <Text>Good Morning</Text>
-            </Card>
-       
-        </View>
-    )
+export default function DashboardScreen() {
+  return (
+    <View style={globalStyles.container}>
+      <Card>
+        <Text>Good Morning</Text>
+      </Card>
+    </View>
+  );
 }
