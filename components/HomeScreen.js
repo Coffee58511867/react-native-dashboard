fff9b8869c0cb61e410a72edff3104d776494905
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-28 11:08:57 +0200

    Created register page

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -25,11 +25,19 @@ export default function HomeScreen({ navigation }) {
       console.log(error);
     }
   };
+  const handleRegister = () => {
+    try {
+      navigation.push("Register");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.text}>Good Morning</Text>
       <CustomButton text="Go to Dashboard" onPress={handleDashboard} />
       <CustomButton text="Login" onPress={handleLogin} />
+      <CustomButton text="Create an Account" onPress={handleRegister} />
       <CustomButton text="Go to Dashboard2" onPress={handleDashboard2} />
     </View>
   );
--- components/Login.js
+++ components/Login.js
@@ -1,37 +1,48 @@
 import React, { useState } from "react";
 import { TextInput, View } from "react-native";
 import { globalStyles } from "../styles/global";
-import Button from '../shared/button'
+import Button from "../shared/button";
+import { SelectList } from "react-native-dropdown-select-list";
 
-export default function Login(){
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const data = [
+    { key: "Canada", value: "Canada" },
+    { key: "England", value: "England" },
+    { key: "Pakistan", value: "Pakistan" },
+    { key: "India", value: "India" },
+    { key: "NewZealand", value: "NewZealand" },
+  ];
+  const [selected, setSelected] = React.useState("");
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleLogin = () => {
-        try {
-              console.log("Done");
-        } catch (error) {
-            console.log(error);
-        }
+  const handleLogin = () => {
+    try {
+      console.log("Done");
+    } catch (error) {
+      console.log(error);
     }
-    return(
-        <View style={globalStyles.container}>
-         <TextInput 
-         style={globalStyles.input}
-         placeholder="Username"
-         keyboardType='email-address'
-         value={username}
-         onChangeText={value => setUsername(value)}
-         />
-                  <TextInput 
-         style={globalStyles.input}
-         placeholder="Password"
-         keyboardType='visible-password'
-         value={password}
-         onChangeText={value => setPassword(value)}
-         />
-         <Button text="Login" onPress={handleLogin}/>
-        </View>
-    )
+  };
+  return (
+    <View style={globalStyles.container}>
+      <TextInput
+        style={globalStyles.input}
+        placeholder="Username"
+        keyboardType="email-address"
+        value={username}
+        onChangeText={(value) => setUsername(value)}
+      />
+      <View style={{width: 310, padding: 5}}> 
+        <SelectList setSelected={setSelected} data={data} style={{}} />
+      </View>
+      <TextInput
+        style={globalStyles.input}
+        placeholder="Password"
+        keyboardType="visible-password"
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+      />
+      <Button text="Login" onPress={handleLogin} />
+    </View>
+  );
 }
