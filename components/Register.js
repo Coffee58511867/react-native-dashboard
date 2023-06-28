a15cf2e552792336f2fe2d441377b7d2dab33992
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-28 10:55:29 +0200

    Created register page

--- components/Register.js
+++ components/Register.js
@@ -2,16 +2,36 @@ import React, { useState , Component} from "react";
 import { TextInput, View } from "react-native";
 import { globalStyles } from "../styles/global";
 import Button from '../shared/button'
+import { SelectList } from "react-native-dropdown-select-list";
 
 export default class App extends React.Component {
   
   render() {
     const [username, setUsername] = useState("");
+    const [phone, setPhone] = useState("");
+    const [district, setDistrict] = useState([
+      {name : "Select your district"},
+      {name : "Berea"},
+      {name : "Botha-Buthe"},
+      {name : "Maseru"},
+      {name : "Mokhotlong"},
+      {name : "Leribe"},
+      {name : "Mafeteng"},
+      {name : "Mohale's Hoek"},
+    ]);
+    const data = [
+      {key:'Canada', value:'Canada'},
+      {key:'England', value:'England'},
+      {key:'Pakistan', value:'Pakistan'},
+      {key:'India', value:'India'},
+      {key:'NewZealand', value:'NewZealand'},
+    ]
     const [password, setPassword] = useState("");
-
+    const [selected, setSelected] = React.useState("");
     const handleLogin = () => {
         try {
               console.log("Done");
+              console.log(selected);
         } catch (error) {
             console.log(error);
         }
@@ -24,8 +44,10 @@ export default class App extends React.Component {
       keyboardType='email-address'
       value={username}
       onChangeText={value => setUsername(value)}
+
       />
-               <TextInput 
+      <SelectList setSelected={setSelected} data={data}  />
+      <TextInput 
       style={globalStyles.input}
       placeholder="Password"
       keyboardType='visible-password'
