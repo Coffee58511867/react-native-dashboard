968e5317dae73048869a8b87384e6234a437be78
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-28 10:44:07 +0200

    Created register page

--- components/Register.js
+++ components/Register.js
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
-import { Text } from "react-native";
+import React, { useState , Component} from "react";
+import { TextInput, View } from "react-native";
+import { globalStyles } from "../styles/global";
+import Button from '../shared/button'
 
-class Register extends Component {
+export default class App extends React.Component {
+  
   render() {
-    return <Text>Hello World!</Text>;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        try {
+              console.log("Done");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return(
+      <View style={{flex: 1, padding: 20}}>
+      <TextInput 
+      style={globalStyles.input}
+      placeholder="Username"
+      keyboardType='email-address'
+      value={username}
+      onChangeText={value => setUsername(value)}
+      />
+               <TextInput 
+      style={globalStyles.input}
+      placeholder="Password"
+      keyboardType='visible-password'
+      value={password}
+      onChangeText={value => setPassword(value)}
+      />
+      <Button text="Login" onPress={handleLogin}/>
+     </View>
+    )
   }
 }
 
